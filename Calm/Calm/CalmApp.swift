583f6860f61b8b37413680f4637930b30b522a13
beratridvanasilturk <br.asltrk@gmail.com>
2024-02-06 00:08:26 +0300

    Added Only Dark Mode

--- Calm/Calm/CalmApp.swift
+++ Calm/Calm/CalmApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct CalmApp: App {
+    @AppStorage("isDarkMode") private var isDarkMode = true
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
